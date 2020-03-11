30e98bb70db9ce8e5007490a2599b8ad7b84e121
Aleia <aleiaknight@gmail.com>
2020-03-10 19:41:29 -0700

    displaying database

--- personal_site/articles/models.py
+++ personal_site/articles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    author = models.TextField()
+    url = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.title
--- personal_site/articles/views.py
+++ personal_site/articles/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import Article
 
 def index(request):
     return render(request, 'base.html')
 
 def articles(request):
-    return render(request, 'articles/articles.html')
+    context={
+        'articles': Article.objects.all()
+    }
+    return render(request, 'articles/articles.html', context)
