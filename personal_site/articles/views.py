42ac6160342b5361f5fc1a3099e2f453d9eb3225
Aleia <aleia@ms-MacBook.local>
2020-03-10 17:07:03 -0700

    added url, template, link, for articles page

--- personal_site/articles/views.py
+++ personal_site/articles/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'base.html')
+
+def articles(request):
+    return render(request, 'articles/articles.html')
--- personal_site/personal_site/urls.py
+++ personal_site/personal_site/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from articles import views
+from articles import views as article_views
 
 urlpatterns = [
-    path('', views.index),
+    path('', article_views.index),
     path('admin/', admin.site.urls),
+    path('articles/', article_views.articles)
 ]
