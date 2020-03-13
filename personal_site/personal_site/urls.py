22355db6b04e4b0f1c55057fd22b6332c04a2259
Aleia <aleiaknight@gmail.com>
2020-03-12 20:27:14 -0700

    added images

--- personal_site/personal_site/urls.py
+++ personal_site/personal_site/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from articles import views as article_views
 from websites import views as website_views
 from code import views as code_views
@@ -27,4 +30,4 @@ urlpatterns = [
     path('websites/', website_views.index),
     path('code/', code_views.index),
     path('robotics/', robotics_views.index)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
