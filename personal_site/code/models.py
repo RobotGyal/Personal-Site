45c8c873ac5a2e023790a7631d85e70ae7f3d4e0
Aleia <aleiaknight@gmail.com>
2020-03-25 00:16:18 -0400

    code page model

--- personal_site/code/models.py
+++ personal_site/code/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class Code(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    img = models.ImageField(upload_to='pics/') 
+    created_on = models.DateTimeField(auto_now_add=True)
+    author = models.TextField()
+    repo_url = models.URLField(max_length=200)
+    live_url = models.URLField(max_length=200)
+    technologies = models.TextField()
+
+
+    def __str__(self):
+        return self.title
--- personal_site/code/views.py
+++ personal_site/code/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from .models import Code
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 # Create your views here.
 def index(request):
-    return render(request, 'code/code_index.html')
+
+    context={
+        'codes': Code.objects.all()
+    }
+    return render(request, 'code/code_index.html', context)
+
+
+# class index(ListView):
+#     """ Renders a list of all Pages. """
+#     model = Code
+
+#     def get(self, request):
+#         """ GET a list of Pages. """
+#         # articles = self.get_queryset().all()
+#         context={
+#             'codes': Code.objects.all()
+#     }
+#         return render(request, '.html', context)
