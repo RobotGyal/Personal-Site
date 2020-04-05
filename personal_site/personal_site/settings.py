f6f5a8cc2856ebf243e2e5991984dfb417e3126b
Aleia <aleiaknight@gmail.com>
2020-04-05 19:30:38 -0400

    heroku

--- personal_site/personal_site/settings.py
+++ personal_site/personal_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a1$$z7ul+p&zpok3^6o_4fk!w@gd0o!kot^veb2^)t$u_k=ms6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['akinventions.herokuapp.com']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,13 +123,26 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 MEDIA_URL = '/statics/pics/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+
+TITLE_MAX_LENGTH = 500
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-TITLE_MAX_LENGTH = 500
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
