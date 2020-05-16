764a56be8f38e3ea560d2ef41515fbfcec31f6b1
Aleia <aleiaknight@gmail.com>
2020-05-15 23:13:04 -0700

    fixed dependecies

--- personal_site/personal_site/settings.py
+++ personal_site/personal_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a1$$z7ul+p&zpok3^6o_4fk!w@gd0o!kot^veb2^)t$u_k=ms6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['akinventions.herokuapp.com']
+ALLOWED_HOSTS = ['akinventions.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -132,7 +132,7 @@ TITLE_MAX_LENGTH = 500
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
@@ -140,9 +140,9 @@ STATICFILES_DIRS = [
 ]
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# import dj_database_url 
+# prod_db  =  dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
